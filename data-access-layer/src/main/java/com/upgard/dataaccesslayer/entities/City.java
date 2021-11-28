26e74b6d7f4f8c68b3e998494cb42c9249dbd53f
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-11-28 18:53:01 +0530

    Todo5: Done with City

---
+++ data-access-layer/src/main/java/com/upgard/dataaccesslayer/entities/City.java
@@ -0,0 +1,31 @@
+package com.upgard.dataaccesslayer.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int city_id ;
+
+    @Column(nullable = false)
+    private String city_name ;
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
+}
