b3307fe48a157a58052f7e4bd5824cf1d4ca4864
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-07 17:27:16 +0530

    TODO 2: Done with Booking Entity Service interface and class

---
+++ data-access-layer/src/main/java/com/upgard/dataaccesslayer/service/BookingSerivceImpl.java
@@ -0,0 +1,43 @@
+package com.upgard.dataaccesslayer.service;
+
+import com.upgard.dataaccesslayer.dao.BookingDao;
+import com.upgard.dataaccesslayer.entities.Booking;
+import com.upgard.dataaccesslayer.exceptions.BookingDetailsNotFoundException;
+import com.upgard.dataaccesslayer.exceptions.MovieDetailsNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service(value = "bookingService")
+public class BookingSerivceImpl implements BookingService{
+
+    @Autowired
+    private BookingDao bookingDao ;
+
+    @Override
+    public Booking acceptBookingDetails(Booking booking) throws MovieDetailsNotFoundException {
+        return bookingDao.save(booking) ;
+
+    }
+
+    @Override
+    public Booking getBookingDetails(int id) throws BookingDetailsNotFoundException {
+        return bookingDao.findById(id)
+                .orElseThrow(
+                        () -> new BookingDetailsNotFoundException("Booking Not Found For Id: " + id)
+                ) ;
+    }
+
+    @Override
+    public boolean deleteBooking(int id) throws BookingDetailsNotFoundException {
+        Booking savedBooking = getBookingDetails(id) ;
+        bookingDao.delete(savedBooking) ;
+        return true;
+    }
+
+    @Override
+    public List<Booking> getAllBookingDetails() {
+        return bookingDao.findAll() ;
+    }
+}
