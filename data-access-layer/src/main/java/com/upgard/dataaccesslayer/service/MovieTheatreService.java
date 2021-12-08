5e62b30c0d835869f2cb8c5386e53f3acd4b8a66
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-08 19:30:42 +0530

    Day 6: Done with TODO 6

--- data-access-layer/src/main/java/com/upgard/dataaccesslayer/service/MovieTheatreService.java
+++ data-access-layer/src/main/java/com/upgard/dataaccesslayer/service/MovieTheatreService.java
@@ -1,7 +1,16 @@
 package com.upgard.dataaccesslayer.service;
 
+import com.upgard.dataaccesslayer.entities.MovieTheatre;
+import com.upgard.dataaccesslayer.exceptions.MovieDetailsNotFoundException;
+import com.upgard.dataaccesslayer.exceptions.TheatreDetailsNotFoundException;
+
+import java.util.List;
+
 public interface MovieTheatreService {
 
-//    public MovieService
+    public MovieTheatre acceptMovieTheatreDetails(MovieTheatre movieTheatre) throws MovieDetailsNotFoundException, TheatreDetailsNotFoundException ;
+    public MovieTheatre getMovieTheatreDetails(int id) throws MovieDetailsNotFoundException ;
+    public boolean deleteMovieTheatre(int id) throws  MovieDetailsNotFoundException ;
+    public List<MovieTheatre> getAllMovieTheatreDetails() ;
 
 }
